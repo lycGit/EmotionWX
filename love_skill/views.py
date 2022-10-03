30bc78fb71cc0c2da77fd27aad3bb2e8f5191182
liuyangcheng <jxpx1989@163.com>
2022-10-03 17:25:12 +0800

    生成文件并跳转URL

--- love_skill/views.py
+++ love_skill/views.py
@@ -1,8 +1,10 @@
 #coding:utf-8
 import os
 
+from werkzeug.utils import redirect
+
 from . import love_skill_bp
-from flask import request, jsonify
+from flask import request, jsonify, Response, send_file
 from utils import  emotion_db_manager
 from flask import render_template
 import json
@@ -32,45 +34,56 @@ def get_love_skill():
 def get_skilldetail():
     artid = request.args.get('artid')
     res = emotion_db_manager.get_love_lesson_detail(int(artid))
-    return render_template("love_skill.html", html_content=res[0][4])
+    out_html = render_template("love_skill.html", html_content=res[0][4])
+    return out_html, "200 Ok", {"Content-type": "text/html"}
+    # response = Response()
+    # response.data = out_html
+    # response.headers = {"Content-type": "text/html"}
+    # return out_html
 
 
-# # 恋爱蜜语
-# @love_skill_bp.route('/skilldetail')
-# def get_skilldetail():
-#     artid = request.args.get('artid')
-#     # 命名生成的html
-#     project_path = os.path.abspath(os.path.dirname(__file__))
-#     file_list_path = project_path + "/html/"
-#     file_path = str(artid) + ".html"
-#     GEN_HTML = file_list_path + file_path
-#     if os.access(GEN_HTML, os.F_OK):
-#         return render_template(GEN_HTML)
-#         # return render_template(("mod1.html",html_content = name))
-#     else:
-#         print('has excute')
-#
-#         # 打开文件，准备写入
-#         f = open(GEN_HTML, 'w')
-#         res = emotion_db_manager.get_love_lesson_detail(int(artid))
-#         # 准备相关变量
-#         # 写入HTML界面中
-#         message = """
-#         <!DOCTYPE html>
-#         <html lang="en">
-#         <head>
-#             <meta charset="UTF-8">
-#             <title>Title</title>
-#         </head>
-#         <body>
-#            %s
-#         </body>
-#         </html>
-#         """ % (res[0][4])
-#         print(GEN_HTML)
-#         # 写入文件
-#         f.write(message)
-#         # 关闭文件
-#         f.close()
-#         # return file_path
-#         return render_template(GEN_HTML)
+# 恋爱蜜语
+@love_skill_bp.route('/skillhtmldetail')
+def get_skill_html_detail():
+    artid = request.args.get('artid')
+    project_path = os.path.abspath(".")
+
+    file_path = str(artid) + ".html"
+    GEN_HTML = project_path + '/static_files/' + file_path
+    # print(GEN_HTML)
+    if os.access(GEN_HTML, os.F_OK):
+        # 发送文件
+        # return send_file('love_skill/html/1698.html', GEN_HTML)
+        # redirectUrl = 'http://127.0.0.1:5000/source/' + file_path
+        redirectUrl = 'https://www.qgsq.space/source/' + file_path
+        # print(redirectUrl)
+        return redirect(redirectUrl)
+    else:
+        print('has excute')
+
+        # 打开文件，准备写入
+        f = open(GEN_HTML, 'w')
+        res = emotion_db_manager.get_love_lesson_detail(int(artid))
+        # 准备相关变量
+        # 写入HTML界面中
+        message = """
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <title>Title</title>
+        </head>
+        <body>
+           %s
+        </body>
+        </html>
+        """ % (res[0][4])
+        print(GEN_HTML)
+        # 写入文件
+        f.write(message)
+        # 关闭文件
+        f.close()
+        # redirectUrl = 'http://127.0.0.1:5000/source/' + file_path
+        redirectUrl = 'https://www.qgsq.space/source/' + file_path
+        # print(redirectUrl)
+        return redirect(redirectUrl)
