e3b595fdbafee3c087055e1f1de1e19eddfc012f
liuyangcheng <jxpx1989@163.com>
2022-10-04 02:17:37 +0800

    修改课程返回字段

--- lesson/views.py
+++ lesson/views.py
@@ -1,50 +1,51 @@
 from . import lesson_bp
 from flask import request, jsonify
 
+
 @lesson_bp.route('/lesson')
 def get_lesson():
     lesson_list = [
         {
             "id": 100,
             "title": "因敏感因素暂停板块，可咨询客服获取相关资料",
-            "wx": "12345678",
-            "lessonCount": "23",
-            "studentsCount": "876"
+            "contact": "12345678",
+            "count": "23",
+            "students": "876"
         },
         {
             "id": 101,
             "title": "因敏感因素暂停板块，可咨询客服获取相关资料",
-            "wx": "12345678",
-            "lessonCount": "23",
-            "studentsCount": "876"
+            "contact": "12345678",
+            "count": "23",
+            "students": "876"
         },
         {
             "id": 102,
             "title": "因敏感因素暂停板块，可咨询客服获取相关资料",
-            "wx": "12345678",
-            "lessonCount": "23",
-            "studentsCount": "876"
+            "contact": "12345678",
+            "count": "23",
+            "students": "876"
         },
         {
             "id": 103,
             "title": "因敏感因素暂停板块，可咨询客服获取相关资料",
-            "wx": "12345678",
-            "lessonCount": "23",
-            "studentsCount": "876"
+            "contact": "12345678",
+            "count": "23",
+            "students": "876"
         },
         {
             "id": 104,
             "title": "因敏感因素暂停板块，可咨询客服获取相关资料",
-            "wx": "12345678",
-            "lessonCount": "23",
-            "studentsCount": "876"
+            "contact": "12345678",
+            "count": "23",
+            "students": "876"
         },
         {
             "id": 105,
             "title": "因敏感因素暂停板块，可咨询客服获取相关资料",
-            "wx": "12345678",
-            "lessonCount": "23",
-            "studentsCount": "876"
+            "contact": "12345678",
+            "count": "23",
+            "students": "876"
         }
     ]
     return jsonify(lesson_list)
